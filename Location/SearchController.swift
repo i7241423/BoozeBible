463413f66a1a02a49c35060bf2b50630a63b1f6f
alexsisan <sisan@live.co.uk>
2017-05-11 16:46:21 +0100

    Search controller

--- Location/SearchController.swift
+++ Location/SearchController.swift
@@ -6,20 +6,40 @@ import SwiftyJSON
 
 class SearchController: UITableViewController {
     
-    
-
-    
-    
     var pub: Pub!
     var pubs = [Pub]()
+    
+    let foodList: [String] = ["No","Yes","Snacks"]
 
-
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var textLabel: UILabel!
+    
     override func viewDidLoad(){
         super.viewDidLoad()
         
-
-        
-       
     }
 
+    @IBAction func FoodView(_ sender: UISegmentedControl) {
+        
+            switch segmentedControl.selectedSegmentIndex
+            {
+            case 0:
+                textLabel.text = foodList[0]
+                
+            case 1:
+                textLabel.text = foodList[1]
+            case 2:
+                textLabel.text = foodList[2]
+            default:
+                break; 
+            }
+        }
+        
+    @IBOutlet weak var SearchButton: UIButton!
 }
+
+//http://46.101.42.98/api/venues/search?food=1 URL TO SEARCH
+
+    
+    
+    
