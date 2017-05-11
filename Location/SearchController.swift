7b728df7ec193c1c7176bb628173e7847c182528
alexsisan <sisan@live.co.uk>
2017-05-11 12:16:44 +0100

    Navigated to search controller

---
+++ Location/SearchController.swift
@@ -0,0 +1,25 @@
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+
+
+class SearchController: UITableViewController {
+    
+    
+
+    
+    
+    var pub: Pub!
+    var pubs = [Pub]()
+
+
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        
+
+        
+       
+    }
+
+}
