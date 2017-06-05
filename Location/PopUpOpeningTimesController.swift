edb02da2275cb5c5cbbeae25f3300bab39a1ad65
alexsisan <sisan@live.co.uk>
2017-06-05 09:34:24 +0100

    Stuff

--- Location/PopUpOpeningTimesController.swift
+++ Location/PopUpOpeningTimesController.swift
@@ -22,8 +22,26 @@ class PopUpOpeningTimesController: UIViewController{
         openingTimesLabel.numberOfLines = 0
         openingTimesLabel.lineBreakMode = NSLineBreakMode.byWordWrapping
         
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        
     }
     
+    func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0{
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
     
+    func keyboardWillHide(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y != 0{
+                self.view.frame.origin.y += keyboardSize.height
+            }
+        }
+    }
     
 }
+
